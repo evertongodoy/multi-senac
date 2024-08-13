146d2eef134adbbb755a7bea3e24454249f052db
Everton Marcos de Godoy <everton.mgodoy@sp.senac.br>
2024-08-13 19:04:07 -0300

    ajustado projeto para commit

--- junit/src/main/java/com/senac/CalculadoraService.java
+++ junit/src/main/java/com/senac/CalculadoraService.java
@@ -1,4 +1,19 @@
 package com.senac;
 
 public class CalculadoraService {
+
+    public String getAcao(String acao){
+        if(acao.equalsIgnoreCase("somar")){
+            return "+";
+        } else if (acao.equalsIgnoreCase("subtrair")) {
+            return "-";
+        } else if (acao.equalsIgnoreCase("dividir")) {
+            return "/";
+        } else if (acao.equalsIgnoreCase("multiplicar")) {
+            return "*";
+        } else {
+            throw new RuntimeException("Acao nao localizada");
+        }
+    }
+
 }
--- junit/src/test/java/com/senac/CalculadoraTest.java
+++ junit/src/test/java/com/senac/CalculadoraTest.java
@@ -1,4 +1,52 @@
 package com.senac;
 
-public class CalculadoraTest {
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CalculadoraTest {
+
+    @Mock
+    private CalculadoraService calculadoraService;
+    @InjectMocks
+    private Calculadora calculadora;
+
+    @Test
+    void testCalcularQuandoSomar(){
+        Integer expected = 20;
+        Mockito.when(calculadoraService.getAcao("somar")).thenReturn("+");
+        var actual = calculadora.calcular("somar", 10, 10);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCalcularQuandoSubtrair(){
+        Integer expected = 0;
+        Mockito.when(calculadoraService.getAcao("subtrair")).thenReturn("-");
+        var actual = calculadora.calcular("subtrair", 10, 10);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCalcularQuandoDividir(){
+        Integer expected = 1;
+        Mockito.when(calculadoraService.getAcao("dividir")).thenReturn("/");
+        var actual = calculadora.calcular("dividir", 10, 10);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCalcularQuandoMultiplicar(){
+        Integer expected = 100;
+        Mockito.when(calculadoraService.getAcao("multiplicar")).thenReturn("*");
+        var actual = calculadora.calcular("multiplicar", 10, 10);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
