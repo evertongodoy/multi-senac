bac8c1c9396433ef114efea172d4a7e90597f010
Everton Marcos de Godoy <everton.mgodoy@sp.senac.br>
2024-08-13 20:05:27 -0300

    adicionado testes unitarios para CalculadoraService
    comentado codigo do jacoco pois nao roda com as veroes atuais de java

--- junit/src/test/java/com/senac/CalculadoraServiceTest.java
+++ junit/src/test/java/com/senac/CalculadoraServiceTest.java
@@ -1,4 +1,47 @@
 package com.senac;
 
-public class CalculadoraServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class CalculadoraServiceTest {
+
+
+    private final CalculadoraService calculadoraService = new CalculadoraService();
+
+    @Test
+    void testGetAcaoSomar() {
+        String actual = calculadoraService.getAcao("somar");
+        var expected = "+";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetAcaoSubtrair() {
+        String actual = calculadoraService.getAcao("subtrair");
+        var expected = "-";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetAcaoMultiplicar() {
+        String actual = calculadoraService.getAcao("multiplicar");
+        var expected = "*";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetAcaoDividir() {
+        String actual = calculadoraService.getAcao("dividir");
+        var expected = "/";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetAcaoThrowException() {
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class,
+                () -> calculadoraService.getAcao("naoExisteAcao"));
+        Assertions.assertEquals("Acao nao localizada", exception.getMessage());
+
+    }
 }
